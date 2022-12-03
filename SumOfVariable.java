2bb9c03d093f56f91dd6c6c9c946bdd4a131a307
Seraj006 <119700034+Seraj006@users.noreply.github.com>
2022-12-03 17:28:05 +0530

    Create SumOfVariable.java

---
+++ SumOfVariable.java
@@ -0,0 +1,13 @@
+// Taking 2 variables 'a' & 'b' and print their sum
+
+import java.util.*;
+
+public class SumOfVariable {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.Out.println(sum);
+  }
+}
+
