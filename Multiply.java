c1c8e1f7e2ce090647ab2c0db8e4a0810035f2e8
Seraj006 <119700034+Seraj006@users.noreply.github.com>
2022-12-03 17:20:30 +0530

    Create Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,12 @@
+// Multiplication of two numbers
+
+public class Multiply {
+  public static void main(String[] args) {
+    
+    int a = 5;
+    int b = 10;
+    
+    int mul = a * b;
+    System.Out.println(mul);
+  }
+}
