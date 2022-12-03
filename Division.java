c483eb60e11879ce188c57af1bb953e61cfec097
Seraj006 <119700034+Seraj006@users.noreply.github.com>
2022-12-03 17:23:17 +0530

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,12 @@
+// Division in Java Programming
+
+public class Division {
+    public static void main(String[] args) {
+           int firstNumber = 6;
+           int secondNumber = 3;
+ 
+           //Now, Divide and display division result
+           System.out.println(firstNumber + " / "+secondNumber +" = " + (firstNumber / secondNumber));
+ 
+    }
+}
